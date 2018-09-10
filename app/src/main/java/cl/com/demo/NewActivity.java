2355b9585e3dcf22dc766c069b60623686d95fa4
chenliang <chenliang4561@163.com>
2018-09-10 15:11:39 +0800

    Added a new Activity

---
+++ app/src/main/java/cl/com/demo/NewActivity.java
@@ -0,0 +1,18 @@
+package cl.com.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by admin on 2018-9-10.
+ */
+
+public class NewActivity extends Activity {
+    
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
