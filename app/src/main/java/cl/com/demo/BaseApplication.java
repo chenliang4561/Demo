15f7f070a9024c5ca804b8d0bf39b0ac17398991
chenliang <chenliang4561@163.com>
2018-09-10 14:57:01 +0800

    no message

---
+++ app/src/main/java/cl/com/demo/BaseApplication.java
@@ -0,0 +1,15 @@
+package cl.com.demo;
+
+import android.app.Application;
+
+/**
+ * Created by admin on 2018-9-10.
+ */
+
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
