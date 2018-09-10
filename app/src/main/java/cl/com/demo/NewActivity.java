8449cf2172e081a096ea9474a0a797ce1eb30165
chenliang4561 <chenliang4561@163.com>
2018-09-10 15:32:09 +0800

    Update NewActivity.java

--- app/src/main/java/cl/com/demo/NewActivity.java
+++ app/src/main/java/cl/com/demo/NewActivity.java
@@ -15,5 +15,6 @@ public class NewActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i("TAG","修改");
+        Log.d("NewActivity","Add a code");
     }
 }
